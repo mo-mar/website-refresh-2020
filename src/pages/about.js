ca6a205300632d885c338b6292124edaaa10b061
Mohamed Omar <50046932+mo-mar@users.noreply.github.com>
2020-10-25 16:20:39 -0400

    [FEAT] adding portfolio page and components (#3)
    * added portfolio page and portfolio piece component
    
    * added recent work components and styles
    
    * paragraph fix on portfolio
    
    * using react helmet for site title
    
    * using propery image imports now
    
    * updating titles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,45 +1,52 @@
 import React from 'react'
 import * as classes from './about.module.scss'
+import headshot from '../assets/mohamed-omar-headshot-square-crop.jpg'
 import Layout from '../components/Layout/Layout'
+import { Helmet } from 'react-helmet'
 
 export default function About() {
   return (
-    <Layout headerText="About me">
-      <div className={classes.container}>
-        <div className="bio">
-          <div className={classes.bioIntro}>
-            <h3>Hello!/!مرحبا </h3>
-            <p>I'm Mohamed, or Mo, or محمد. </p>
-            <p>
-              I'm a front-end developer based in Toronto who, at the time of
-              writing, still gets giddy when a line of code makes the computer
-              or browser do something.
-            </p>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mohamed Omar | Front-end developer</title>
+        <link rel="canonical" href="http://thatmohamedomar.com" />
+      </Helmet>
+      <Layout headerText="About me">
+        <div className={classes.container}>
+          <div className="bio">
+            <div className={classes.bioIntro}>
+              <h3>Hello!/!مرحبا </h3>
+              <p>I'm Mohamed, or Mo, or محمد. </p>
+              <p>
+                I'm a front-end developer based in Toronto who, at the time of
+                writing, still gets giddy when a line of code makes the computer
+                or browser do something.
+              </p>
+            </div>
+            <div className={classes.bioBody}>
+              <p>
+                Before I caught JavaScript fever, I worked in journalism and
+                online content creation. I got to talk to and write about all
+                kinds of people doing all kinds of important, fascinating,
+                hilarious, stupid and scary things. (I'll never forget the guy
+                who{' '}
+                <a
+                  href="https://www.huffingtonpost.ca/2016/07/05/rick-nelson-bear-puncher_n_10820386.html"
+                  rel="noopener noreferrer nofollow"
+                  target=" _blank"
+                >
+                  punched a bear
+                </a>
+                . Good times.)
+              </p>
+            </div>
           </div>
-          <div className={classes.bioBody}>
-            <p>
-              Before I caught JavaScript fever, I worked in journalism and
-              online content creation. I got to talk to and write about all
-              kinds of people doing all kinds of important, fascinating,
-              hilarious, stupid and scary things. (I'll never forget the guy who{' '}
-              <a
-                href="https://www.huffingtonpost.ca/2016/07/05/rick-nelson-bear-puncher_n_10820386.html"
-                rel="noopener noreferrer nofollow"
-                target=" _blank"
-              >
-                punched a bear
-              </a>
-              . Good times.)
-            </p>
+          <div className={classes.headshot}>
+            <img src={headshot} alt="Mohamed Omar photograph" />
           </div>
         </div>
-        <div className={classes.headshot}>
-          <img
-            src="../assets/mohamed-omar-headshot-square-crop.jpg"
-            alt="Mohamed Omar photograph"
-          />
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
---
+++ src/pages/portfolio.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+import { Helmet } from 'react-helmet'
+import * as classes from './portfolio.module.scss'
+import whosMyRepImage from '../assets/whos-my-rep-screenshot.png'
+import kanbanBoardImage from '../assets/kanban-board.png'
+import myWebsiteImage from '../assets/website-screenshot.png'
+import weatherAppImage from '../assets/weather-app.png'
+import PortfolioPiece from '../components/PortfolioPiece/PortfolioPiece'
+
+const whosMyRep = {
+  name: `Who's My Rep?`,
+  link: 'https://mo-mar.github.io/mohamedOmarProjectFive/',
+  image: whosMyRepImage,
+  description: `A single-page app that connects Canadians with their municipal, provincial and federal political representatives. All data pulled in from the Open North API.`,
+  githubLink: 'https://github.com/mo-mar/mohamedOmarProjectFive',
+}
+
+const kanbanBoard = {
+  name: 'Kanban Board',
+  link: 'https://the-kanban-board.netlify.app/',
+  image: kanbanBoardImage,
+  description:
+    'A simple Kanban Board built with React and React Beautiful DnD.',
+  githubLink: 'https://github.com/mo-mar/kanban-board',
+}
+
+const weatherApp = {
+  name: 'My Generic Weather App',
+  link: 'https://mo-mar.github.io/weather-app/',
+  image: weatherAppImage,
+  description: `It's a weather app. Does it reinvent the wheel? No. Was it a great way to practice React hooks? You betcha.`,
+  githubLink: 'https://github.com/mo-mar/weather-app',
+}
+
+const myWebsite = {
+  name: 'This very website',
+  link: null,
+  image: myWebsiteImage,
+  description: `My personal website and blog were built with Gatsby.js, SASS and CSS modules, which is a bit of an upgrade from the plain HTML page I made last year!`,
+  githubLink: 'https://github.com/mo-mar/website-refresh-2020',
+}
+
+export default function Portfolio() {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mohamed Omar | Front-end developer</title>
+        <link rel="canonical" href="http://thatmohamedomar.com" />
+      </Helmet>
+      <Layout headerText="Recent work">
+        <div className={classes.intro}>
+          <p>
+            I do my best to frequently build new apps and websites to learn new
+            technologies and keep my tools sharp.{' '}
+          </p>
+          <p>
+            Some of them will land here, some of them won't. They can't all be
+            bangers.
+          </p>
+          <p>
+            Feel free to check out my GitHub profile if you want{' '}
+            <a
+              href="https://github.com/mo-mar"
+              rel="noopener noreferrer nofollow"
+              target=" _blank"
+            >
+              a peek
+            </a>{' '}
+            behind the curtain.
+          </p>
+        </div>
+        <div className={classes.portfolioGrid}>
+          <PortfolioPiece portfolioPieceData={whosMyRep} />
+          <PortfolioPiece portfolioPieceData={kanbanBoard} />
+          <PortfolioPiece portfolioPieceData={weatherApp} />
+          <PortfolioPiece portfolioPieceData={myWebsite} />
+        </div>
+      </Layout>
+    </>
+  )
+}
