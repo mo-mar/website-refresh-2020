8d829bcfa703b7f1de2c1687b0879caff6922e8d
Mohamed Omar <mohamedsomar1@gmail.com>
2020-10-25 14:53:41 -0400

    added recent work components and styles

--- src/components/PortfolioPiece/PortfolioPiece.js
+++ src/components/PortfolioPiece/PortfolioPiece.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import * as classes from './portfoliopiece.module.scss'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faReact } from '@fortawesome/free-brands-svg-icons'
 export default function PortfolioPiece({ portfolioPieceData }) {
   return (
     <div className={classes.container}>
-      <h3>{portfolioPieceData.name}</h3>
-      <div className={classes.imageContainer}>
-        <a href={portfolioPieceData.link} target="_blank">
-          <img
-            src={portfolioPieceData.image}
-            alt="A screengrab of a web app called Who's My Rep?, built by Mohamed Omar."
-          />
-        </a>
+      <div className={classes.portfolioBody}>
+        <h3>{portfolioPieceData.name}</h3>
+        <div className={classes.imageContainer}>
+          <a
+            href={portfolioPieceData.link}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            target=" _blank"
+          >
+            <img
+              src={portfolioPieceData.image}
+              alt="A screengrab of a web app called Who's My Rep?, built by Mohamed Omar."
+            />
+          </a>
+        </div>
+        <p>{portfolioPieceData.description}</p>
       </div>
-      <p>{portfolioPieceData.description}</p>
-
-      <p>github link</p>
-      <p>live website link...</p>
+      <a
+        href={portfolioPieceData.githubLink}
+        rel="noopener noreferrer nofollow"
+        target=" _blank"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
     </div>
   )
 }
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -11,22 +11,57 @@ const whosMyRep = {
   githubLink: 'https://github.com/mo-mar/mohamedOmarProjectFive',
 }
 
+const kanbanBoard = {
+  name: 'Kanban Board',
+  link: 'https://the-kanban-board.netlify.app/',
+  image: '../../assets/kanban-board.png',
+  description:
+    'A simple Kanban Board built with React and React Beautiful DnD.',
+  githubLink: 'https://github.com/mo-mar/kanban-board',
+}
+
+const weatherApp = {
+  name: 'My Generic Weather App',
+  link: 'https://mo-mar.github.io/weather-app/',
+  image: '../../assets/weather-app.png',
+  description: `It's a weather app. Does it reinvent the wheel? No. Was it a great way to practice React hooks? You betcha.`,
+  githubLink: 'https://github.com/mo-mar/weather-app',
+}
+
+const myWebsite = {
+  name: 'This very website',
+  link: null,
+  image: '../../assets/website-screenshot.png',
+  description: `My personal website and blog were built with Gatsby.js, SASS and CSS modules, which is a bit of an upgrade from the plain HTML page I made last year!`,
+  githubLink: 'https://github.com/mo-mar/website-refresh-2020',
+}
+
 export default function Portfolio() {
   return (
     <Layout headerText="Recent work">
-      <p>
-        I try to build as many apps and websites as I can to sharpen my skills
-        and learn new technologies, no matter how silly or simple they may seem
-        on the surface. From a weather app that taught me the power of React's
-        useEffect hook or a Kanban board that showed me how awesome Styled
-        Components can be, this little collection of my recent work is a good
-        sample of what I've been up to lately.
-      </p>
+      <div className={classes.intro}>
+        <p>
+          I do my best to frequently build new apps and websites to learn new
+          technologies and keep my tools sharp. Some of them will land here,
+          some of them won't. They can't all be bangers.
+        </p>
+        <p>
+          Feel free to check out my Github profile if you want{' '}
+          <a
+            href="https://github.com/mo-mar"
+            rel="noopener noreferrer nofollow"
+            target=" _blank"
+          >
+            a peek
+          </a>{' '}
+          behind the curtain.
+        </p>
+      </div>
       <div className={classes.portfolioGrid}>
         <PortfolioPiece portfolioPieceData={whosMyRep} />
-        <PortfolioPiece portfolioPieceData={whosMyRep} />
-        <PortfolioPiece portfolioPieceData={whosMyRep} />
-        <PortfolioPiece portfolioPieceData={whosMyRep} />
+        <PortfolioPiece portfolioPieceData={kanbanBoard} />
+        <PortfolioPiece portfolioPieceData={weatherApp} />
+        <PortfolioPiece portfolioPieceData={myWebsite} />
       </div>
     </Layout>
   )
