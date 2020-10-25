20079d17f45cf1b6d9a02554863e62259db765ed
Mohamed Omar <mohamedsomar1@gmail.com>
2020-10-25 15:50:27 -0400

    using react helmet for site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { Helmet } from 'react-helmet'
 
 export default function Home() {
   return (
-    <Layout headerText="Mohamed Omar" letterColour="#2f6cb1">
-      <p>What a world.</p>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mohamed Omar | Front-end developer</title>
+        <link rel="canonical" href="http://thatmohamedomar.com" />
+      </Helmet>
+      <Layout headerText="Mohamed Omar" letterColour="#2f6cb1">
+        <p>What a world.</p>
 
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </Layout>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      </Layout>
+    </>
   )
 }
