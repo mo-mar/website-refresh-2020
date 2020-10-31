a00f8803cea55b6f460b324fc97be905148c9d67
Mohamed Omar <mohamedsomar1@gmail.com>
2020-10-31 16:30:17 -0400

    another fix for mixed content

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ export default function About() {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Mohamed Omar | Front-end developer</title>
-        <link rel="canonical" href="http://thatmohamedomar.com" />
+        <link rel="canonical" href="https://thatmohamedomar.com" />
       </Helmet>
       <Layout headerText="About me">
         <div className={classes.container}>
